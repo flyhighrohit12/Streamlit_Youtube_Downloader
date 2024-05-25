5b7c651c4fff622d0a5163f1940af2fd6f39630e
flyhighrohit12 <flyhighrohit12@gmail.com>
2024-05-25 13:12:00 +0530

    Update youtube_downloader.py

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -1,28 +1,33 @@
 import streamlit as st
 from pytube import YouTube
-import os
+import tempfile
 
-# Title of the application
 st.title('YouTube Video Downloader')
 
-# Taking the URL of the YouTube video from the user
-url = st.text_input('Enter the URL of the YouTube video you wish to download:', '')
+# Text input for the YouTube video URL
+url = st.text_input('Enter the URL of the YouTube video you wish to download:')
 
 if url:
+    # Initialize YouTube object with the URL
     yt = YouTube(url)
     st.write(f"**Video Title:** {yt.title}")
-    st.write(f"**Video Length:** {yt.length // 60} minutes {yt.length % 60} seconds")
-    st.write(f"**Number of Views:** {yt.views}")
     st.image(yt.thumbnail_url)
 
-    # Let the user choose the stream
+    # Filtering available video streams
     stream = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc()
     options = [(i.itag, i.resolution) for i in stream]
-    itag = st.selectbox('Choose the quality/resolution of the video to download:', options, format_func=lambda x: x[1])
+    selected_option = st.selectbox('Choose the quality/resolution of the video to download:', options, format_func=lambda x: x[1])
 
     if st.button('Download Video'):
-        selected_stream = stream.get_by_itag(itag[0])
-        with st.spinner('Downloading...'):
-            selected_stream.download()
-        st.success('Downloaded Successfully!')
-        st.balloons()
+        selected_stream = stream.get_by_itag(selected_option[0])
+        with tempfile.NamedTemporaryFile(delete=False) as tmpfile:
+            # Download the video to a temporary file
+            selected_stream.download(output_path=tmpfile.name, filename=yt.title)
+            tmpfile.flush()
+            
+            # Reopen the temporary file and create a download button
+            with open(tmpfile.name + '.mp4', 'rb') as f:
+                st.download_button(label="Download Video",
+                                   data=f,
+                                   file_name=yt.title + ".mp4",
+                                   mime="video/mp4")
