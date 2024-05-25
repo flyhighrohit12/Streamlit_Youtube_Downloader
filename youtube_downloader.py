6a2e6f4a3d1808263db2dc60f4623225cc56e32f
flyhighrohit12 <flyhighrohit12@gmail.com>
2024-05-25 13:16:54 +0530

    Update youtube_downloader.py

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -1,33 +1,35 @@
 import streamlit as st
 from pytube import YouTube
 import tempfile
+import os
 
 st.title('YouTube Video Downloader')
 
-# Text input for the YouTube video URL
 url = st.text_input('Enter the URL of the YouTube video you wish to download:')
 
 if url:
-    # Initialize YouTube object with the URL
     yt = YouTube(url)
     st.write(f"**Video Title:** {yt.title}")
     st.image(yt.thumbnail_url)
 
-    # Filtering available video streams
     stream = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc()
     options = [(i.itag, i.resolution) for i in stream]
     selected_option = st.selectbox('Choose the quality/resolution of the video to download:', options, format_func=lambda x: x[1])
 
     if st.button('Download Video'):
         selected_stream = stream.get_by_itag(selected_option[0])
-        with tempfile.NamedTemporaryFile(delete=False) as tmpfile:
-            # Download the video to a temporary file
-            selected_stream.download(output_path=tmpfile.name, filename=yt.title)
-            tmpfile.flush()
-            
-            # Reopen the temporary file and create a download button
-            with open(tmpfile.name + '.mp4', 'rb') as f:
+
+        # Sanitize the title for use as a filename
+        safe_filename = yt.title.replace('/', '-').replace('\\', '-').replace(':', '-')
+
+        # Use a temporary file directory context to handle the file
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            file_path = os.path.join(tmpdirname, safe_filename + '.mp4')
+            selected_stream.download(output_path=tmpdirname, filename=safe_filename)
+
+            # Open the file and create a download button
+            with open(file_path, 'rb') as f:
                 st.download_button(label="Download Video",
                                    data=f,
-                                   file_name=yt.title + ".mp4",
+                                   file_name=safe_filename + ".mp4",
                                    mime="video/mp4")
